f416795fba06a43a8489156c6619308de804047e
Mohamed Magdy <mohamedmagdy31098@gmail.com>
2025-07-21 12:56:01 +0300

    Implementation of 2D Array

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -23,7 +23,7 @@
             #region 1D Array
 
             //int[] numbers = [1, 2, 3];
-            
+
             //numbers[0] = 1;
             //numbers[1] = 2; 
             //numbers[2] = 3; 
@@ -39,7 +39,51 @@
 
             #endregion
 
+            #region 2D Array
+    //int[,] marks = new int[2,5];
 
+    //Console.WriteLine("Please Enter Values of an Array .");
+
+    //        for (int i = 0; i <marks.GetLength(0); i++)
+    //        {
+    //            Console.WriteLine($"marks of student number{1+i} .");
+    //            for (int j = 0; j <marks.GetLength(1);)
+    //            {
+    //                Console.Write($"Subject of number [{j + 1}] : "); 
+    //               bool isParsed = int.TryParse(Console.ReadLine(), out marks[i,j]);
+    //                if (isParsed )
+    //                {
+    //                    j++;
+    //                }
+    //                else
+    //                {
+    //                    Console.WriteLine("Please enter number in valid format");
+    //                }
+
+
+    //            }
+    //            Console.WriteLine("=====================");
+                
+    //        }
+
+    //        Console.Clear();
+
+    //        for (int i = 0; i < marks.GetLength(0); i++)
+    //        {
+    //            Console.WriteLine($"Marks of student number {i+1}");
+
+    //            for (int j = 0; j < marks.GetLength(1); j++)
+    //            {
+    //                Console.WriteLine($"subject number{j+1} >= {marks[i,j]} ");
+                    
+    //            }
+    //            Console.WriteLine("=====================");
+    //        }
+
+
+
+
+            #endregion
 
 
 
