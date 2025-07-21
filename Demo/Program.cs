f976b4cd11a6d455e0c3322df7ed440ecde7b003
Mohamed Magdy <mohamedmagdy31098@gmail.com>
2025-07-21 11:41:43 +0300

    Implementation of 1D Array

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Arrays
+
+            #region 1D Array Ways
+
+            //int[] numbers01;
+            //numbers01 = new int[5];
+
+            //int [] numbers02 = new int[5] {1,2,3,4,5};
+
+            //int[] numbers03 = {1,2,3,4,5};
+
+            //int[] numbers04 = [1,2,4,5];
+
+
+            #endregion
+
+            #region 1D Array
+
+            //int[] numbers = [1, 2, 3];
+            
+            //numbers[0] = 1;
+            //numbers[1] = 2; 
+            //numbers[2] = 3; 
+
+            //Console.WriteLine(numbers);
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers[2]);
+
+            //Console.WriteLine(numbers.Length); // length of array
+            //Console.WriteLine(numbers.Rank); // number of dimensions through array
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
         }
     }
 }
