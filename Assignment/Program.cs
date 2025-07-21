925c24334b1449e82be3bbd524b535ef14c5c2cd
Mohamed Magdy <mohamedmagdy31098@gmail.com>
2025-07-21 17:11:04 +0300

    Array - Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Arrays
+
+            #region Q1
+
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+                
+            //}
+
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region Q2
+
+            #endregion
+
+            #region Q3
+
+            #endregion
+
+
+            #region Q4
+
+            #endregion
+
+
+            #region Q5
+
+            #endregion
+
+
+            #region Q6
+
+            #endregion
+
+
+            #region Q7
+
+            #endregion
+
+
+            #region Q8
+
+            #endregion
+
+            #endregion
+
+
+
+
+
+
+
+
         }
     }
 }
