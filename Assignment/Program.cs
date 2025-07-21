d93ab9ac0b742529027962051252e576480339d1
Mohamed Magdy <mohamedmagdy31098@gmail.com>
2025-07-22 01:03:36 +0300

    Array - Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment
+﻿using System;
+
+namespace Assignment
 {
     internal class Program
     {
@@ -16,7 +18,7 @@
             //for (int i = 0; i < numbers.Length; i++)
             //{
             //    sum += numbers[i];
-                
+
             //}
 
             //Console.WriteLine(sum);
@@ -24,6 +26,31 @@
             #endregion
 
             #region Q2
+
+            //const int size = 3; // for simplicity but we can take all information from user
+            
+            //int[] arr01 =  [3,1,2];  
+          
+            //int[] arr02 = [6, 4, 5];
+            
+            //int[] MergedArray = new int [2 * size] ;
+            //for (int i = 0; i < size; i++)
+            //{
+            //    MergedArray[i] = arr01[i];
+            //    MergedArray[i + size] = arr02[i];
+            //}
+
+            //Array.Sort(MergedArray);
+
+            //for (int i = 0; i < 2*size; i++)
+            //{
+
+            //    Console.Write(MergedArray[i] + " ");
+
+            //}
+
+            //Console.WriteLine();
+
 
             #endregion
 
@@ -55,6 +82,15 @@
             #region Q8
 
             #endregion
+
+            #region Q9
+
+            #endregion
+
+            #region Q10
+
+            #endregion
+
 
             #endregion
 
