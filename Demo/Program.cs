9b989489e17f6860be1a912a9624d41e48021592
Mohamed Magdy <mohamedmagdy31098@gmail.com>
2025-07-21 15:01:30 +0300

    Implementation of 2D Array One Loop

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -40,52 +40,83 @@
             #endregion
 
             #region 2D Array
-    //int[,] marks = new int[2,5];
+            //int[,] marks = new int[2,5];
 
-    //Console.WriteLine("Please Enter Values of an Array .");
+            //Console.WriteLine("Please Enter Values of an Array .");
 
-    //        for (int i = 0; i <marks.GetLength(0); i++)
-    //        {
-    //            Console.WriteLine($"marks of student number{1+i} .");
-    //            for (int j = 0; j <marks.GetLength(1);)
-    //            {
-    //                Console.Write($"Subject of number [{j + 1}] : "); 
-    //               bool isParsed = int.TryParse(Console.ReadLine(), out marks[i,j]);
-    //                if (isParsed )
-    //                {
-    //                    j++;
-    //                }
-    //                else
-    //                {
-    //                    Console.WriteLine("Please enter number in valid format");
-    //                }
+            //        for (int i = 0; i <marks.GetLength(0); i++)
+            //        {
+            //            Console.WriteLine($"marks of student number{1+i} .");
+            //            for (int j = 0; j <marks.GetLength(1);)
+            //            {
+            //                Console.Write($"Subject of number [{j + 1}] : "); 
+            //               bool isParsed = int.TryParse(Console.ReadLine(), out marks[i,j]);
+            //                if (isParsed )
+            //                {
+            //                    j++;
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("Please enter number in valid format");
+            //                }
 
 
-    //            }
-    //            Console.WriteLine("=====================");
-                
-    //        }
+            //            }
+            //            Console.WriteLine("=====================");
 
-    //        Console.Clear();
+            //        }
 
-    //        for (int i = 0; i < marks.GetLength(0); i++)
-    //        {
-    //            Console.WriteLine($"Marks of student number {i+1}");
+            //        Console.Clear();
 
-    //            for (int j = 0; j < marks.GetLength(1); j++)
-    //            {
-    //                Console.WriteLine($"subject number{j+1} >= {marks[i,j]} ");
-                    
-    //            }
-    //            Console.WriteLine("=====================");
-    //        }
+            //        for (int i = 0; i < marks.GetLength(0); i++)
+            //        {
+            //            Console.WriteLine($"Marks of student number {i+1}");
+
+            //            for (int j = 0; j < marks.GetLength(1); j++)
+            //            {
+            //                Console.WriteLine($"subject number{j+1} >= {marks[i,j]} ");
+
+            //            }
+            //            Console.WriteLine("=====================");
+            //        }
 
 
 
 
             #endregion
 
+            #region 2D Array One For Loop
 
+            //int[,] marks = new int [2, 4];
+
+            //for (int i = 0; i < marks.Length; )
+            //{
+            //  bool isParsed = int.TryParse(Console.ReadLine(), out int StudentGrade);
+
+
+            //    if (isParsed)
+            //    {
+            //        i++;
+            //        marks[i / marks.GetLength(1), i % marks.GetLength(1)] = StudentGrade;
+            //    }
+
+            //    else
+            //        Console.WriteLine("Please enter number in valid format");
+
+            //}
+
+            //Console.Clear();
+
+            //Console.WriteLine("Marks Values");
+
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    Console.Write(marks[i / marks.GetLength(1), i % marks.GetLength(1)] + " ");
+            //}
+
+
+
+            #endregion
 
 
 
